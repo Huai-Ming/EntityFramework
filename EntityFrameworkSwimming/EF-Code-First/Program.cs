584822192c581fc31d57dc52cac89b8d7cac10a9
Hyman <1604723751@qq.com>
2017-11-23 10:04:57 +0800

    add code first sample

---
+++ EntityFrameworkSwimming/EF-Code-First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Code_First.Entity;
+
+namespace EF_Code_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                Student stud = new Student() { StudentName = "New Student" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.Write("Done");
+            Console.Read();
+        }
+    }
+}
